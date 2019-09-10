f14436d4a5c23c007120afa9adfbf81ff7ee9033
Javakhaa <49414528+Javakhaa@users.noreply.github.com>
2019-09-10 17:21:23 +0400

    initial commit

---
+++ TDLView1/Models/RegistrationViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TDLView1.Models
+{
+    public class RegistrationViewModel
+    {
+        [Required(ErrorMessage = "Employee {0} is required")]
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string RepeatPassword { get; set; }
+
+    }
+}
