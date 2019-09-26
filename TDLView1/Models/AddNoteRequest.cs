a9b5bb5cf474bfd66c50c3d8eef4fa9b52c7dc9a
Javakhaa <49414528+Javakhaa@users.noreply.github.com>
2019-09-26 12:20:00 +0400

    Changed View organize , added models for note action

---
+++ TDLView1/Models/AddNoteRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TDLView1.Models
+{
+    public class AddNoteRequest
+    {
+        public string Description { get; set; }
+    }
+}
